f3ce7657f5ae94444230096790e4315e5936dacc
Maro Park <maro.prk@gmail.com>
2024-05-15 18:04:29 -0700

    added style to give border & shadow

--- src/components/tags/tagPicker.js
+++ src/components/tags/tagPicker.js
@@ -2,10 +2,24 @@ import React from 'react';
 import TagsContainer from './TagsContainer';
 
 const TagPicker = ({ initialTags, allTags, userTags, onTagAssigned, onTagRemoved }) => {
+  const tagPickerStyle = {
+    border: '1px solid #ddd',
+    borderRadius: '4px',
+    padding: '16px',
+    width: '100%',
+    boxSizing: 'border-box',
+    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)'
+  };
 
   return (
-    <div className="tag-picker">
-      <TagsContainer tags={initialTags} allTags={allTags} userTags={userTags} onRemove={onTagRemoved} onTagAssigned={onTagAssigned} />
+    <div className="tag-picker" style={tagPickerStyle}>
+      <TagsContainer
+        tags={initialTags}
+        allTags={allTags}
+        userTags={userTags}
+        onRemove={onTagRemoved}
+        onTagAssigned={onTagAssigned}
+      />
     </div>
   );
 };
